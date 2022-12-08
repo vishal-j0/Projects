60fcae2ddaef99ec42c7c1d923dc8114b860b7ba
vishal-j0 <vishaljadhav12454@gmail.com>
2022-12-09 05:06:23 +0530

    VDBS First Project

---
+++ UploadMultiples/UploadMultiples/CompanyMaster.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CompanyMaster.aspx.cs" Inherits="UploadMultiples.fileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label2" runat="server" Text="Enter Your Name"></asp:Label>  <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+             <asp:FileUpload ID="FileUpload1" AllowMultiple="true" runat="server" /> <br />
+            <asp:Button ID="Button1" runat="server"  Text="Submit" OnClick="Button1_Click" /> <br />
+            <asp:Label ID="Label1" runat="server" Text=" "></asp:Label>
+            
+            
+           
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ UploadMultiples/UploadMultiples/CompanyMaster.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace UploadMultiples
+{
+    public partial class fileUpload : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+   
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into CompanyMaster values(@name,@status,@path)";
+            con.Open();
+            Label1.Text = "";
+            if (FileUpload1.HasFiles)
+            {
+                foreach(HttpPostedFile fu in FileUpload1.PostedFiles)
+                {
+                    string path ="/images/" +fu.FileName;
+                    cmd.Parameters.AddWithValue("@name", fu.FileName);
+                    cmd.Parameters.AddWithValue("@path", path);
+                    if(cmd.ExecuteNonQuery()>0)
+                    {
+                        fu.SaveAs(Path.Combine(Server.MapPath("/images"), fu.FileName));
+                        Label1.Text += string.Format("{0}<br>", fu.FileName+ "Uploaded and save to Database");
+                        cmd.Parameters.Clear();
+                    }
+                    
+                   
+                }
+
+            }else
+            {
+                Label1.Text = "Select a file First";
+            }
+        }
+    }
+}
